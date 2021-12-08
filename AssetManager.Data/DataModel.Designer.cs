b074bdfc07d7a75a1fbacc76e3ed052179b823c3
Jesse Milam <jessemilam@gmail.com>
2021-12-08 16:08:21 -0500

    Updated targeted .NET framework for all projects.

--- AssetManager.Data/DataModel.Designer.cs
+++ AssetManager.Data/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\FS118341\source\repos\AssetManagerAPI\AssetManager.Data\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Jesse Milam\source\repos\AssetManager\AssetManager.Data\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
